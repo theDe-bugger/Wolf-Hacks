39515c9df58d154d17828302c6a9161a202d438c
TheDe-bugger <jaimildalwadi2003@gmail.com>
2021-04-11 14:59:25 -0400

    working on forms

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function health(){
+    var final = ""
+    final = document.getElementById('diseases').submit();
+    
+    alert(JSON.stringify(final))
+}
+
+function mental(){
+
+}
+function nutrition(){
+
+}
